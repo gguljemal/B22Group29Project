5dead412a86580c130359e7dae71077bff155d79
B22-User <gguljemalg@gmail.com>
2021-07-01 14:19:52 -0400

    user story 102 has been started

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,7 @@
+public class UserStory102 {
+
+    //lots of code has been written here
+    //ready to push this branch to remote
+    //and open pull request so I can start collaborating
+    // so my team member can comment, review and approve or request for more changes
+}
