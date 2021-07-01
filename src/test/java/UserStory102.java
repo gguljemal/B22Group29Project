f063403fc50e22f42b807fa74562a2474def08c1
B22-User <gguljemalg@gmail.com>
2021-07-01 14:35:44 -0400

    user story 102 added 1 more line

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -4,4 +4,5 @@ public class UserStory102 {
     //ready to push this branch to remote
     //and open pull request so I can start collaborating
     // so my team member can comment, review and approve or request for more changes
+    // more changes added as request
 }
