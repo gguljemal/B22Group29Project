e4f4f9ee2f5c993d58ababdb354872cd7f98b422
asuman <ozarasuman@gmail.com>
2021-07-02 13:47:54 -0400

    first test commit with group

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -5,4 +5,7 @@ public class UserStory102 {
     //and open pull request so I can start collaborating
     // so my team member can comment, review and approve or request for more changes
     // more changes added as request
+    public static void main(String[] args) {
+        System.out.println("Test it");
+    }
 }
