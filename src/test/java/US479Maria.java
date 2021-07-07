264227f1ffc260296b252d0d2ff42be6bba721d6
Maria <prizmaria@gmail.com>
2021-07-07 19:58:09 -0400

    US479 Comment 2 added

--- src/test/java/US479Maria.java
+++ src/test/java/US479Maria.java
@@ -3,4 +3,5 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class US479Maria {
     //hello world
     // trying more - not understanding it really :-)
+    //do you guys understand this???
 }
