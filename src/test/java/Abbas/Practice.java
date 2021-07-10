9ebab5b5825151d5d2b9ee9d62af150cbc88ef74
Abbas Verdiyev <Abverdi2320@gmail.com>
2021-07-10 17:51:50 -0400

    Just testing

---
+++ src/test/java/Abbas/Practice.java
@@ -0,0 +1,23 @@
+package Abbas;
+//
+//public class Practice {
+//    public static boolean main(String[] args){
+//    }
+//}
+
+//                System.out.println(setUp("civic"));
+//
+//
+//            public static boolean setUp(String word){
+//                boolean isPalindrome = true;
+//                for (int i = 0; i < word.length()/2; i++) {
+//                    if (word.charAt(i)!=word.charAt(word.length()-1-i)){
+//                        isPalindrome = false;
+//                    }
+//                }
+//                return isPalindrome;
+//            }
+//        }
+//
+//    }
+
